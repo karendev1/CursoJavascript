201a41159b2ea55d15377b379cc2e91ff6ec21d6
Karen Christina <karen.santana@santander.com.br>
2021-09-29 07:48:45 -0300

    tratando e lançando erros

---
+++ tratamentoErros/assets/js/main.js
@@ -0,0 +1,25 @@
+try {
+  // Tente executar o código
+  console.log(naoExisto);
+} catch (err) {
+  console.log("Serviço indisponivel. Tente novamente mais tarde.");
+}
+
+// ---------------------------------------------
+
+function soma(x, y) {
+  if (typeof x !== "number" || typeof y !== "number") {
+    throw new Error("x e y precisam ser numeros");
+  }
+
+  return x + y;
+}
+
+try{
+    console.log(soma(1, 2))
+    console.log(soma('1', 2));
+}catch(error){
+    console.log("Serviço indisponivel. Tente novamente mais tarde.")
+}
+
+

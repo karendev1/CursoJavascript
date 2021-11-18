59bdf9a39b9e951312d2e9761fdab7df1e3f227e
Karen Christina <karen.santana@santander.com.br>
2021-11-18 08:07:04 -0300

    método splice

---
+++ metodoSplice/assets/js/main.js
@@ -0,0 +1,14 @@
+const nomes = ["Maria", "João", "Eduardo", "Gabriel", "Julia"];
+
+// nomes.splice(indice, delete, elem1... elemN)
+
+// pop
+// const removidos = nomes.splice(3, 0, "Luiz", 'Otávio');
+
+// shift
+// const removidos = nomes.splice(0, 1)
+
+// push
+// nomes.splice(nomes.length, 0, "Karen")
+// console.log(nomes);
+

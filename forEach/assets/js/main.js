e63899ac4c112792d43278942a38d06b6ecdab9a
Karen Christina <karen.santana@santander.com.br>
2021-11-22 08:59:12 -0300

    forEach

---
+++ forEach/assets/js/main.js
@@ -0,0 +1,10 @@
+// For each só está disponivel dentro de Arrays!
+
+const a1 = [1, 2, 3, 4, 5, 6, 7, 8, 9];
+let total = 0;
+
+a1.forEach((valor, indice, array) => {
+    total += valor
+})
+
+console.log(total)

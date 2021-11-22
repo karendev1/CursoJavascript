b4aa41c946b35cb6432dbf4e9e8abbde6481ef8b
Karen Christina <karen.santana@santander.com.br>
2021-11-22 07:43:51 -0300

    filter + map + reduce

--- metodoReduce/assets/js/main.js
+++ metodoReduce/assets/js/main.js
@@ -4,8 +4,25 @@
 const numeros = [5, 50, 80, 1, 2, 3, 5, 8, 7, 11, 15, 22, 27];
 
 const total = numeros.reduce((acumulador, valor) => {
-    acumulador += valor
-    return acumulador
-})
+  acumulador += valor;
+  return acumulador;
+});
 
-console.log(total)
+console.log(total);
+
+// ------------------------------
+
+// Retorne a soma do dobro de todos os pares
+//  -> Filtrar pares
+// -> Dobrar valores
+// -> Reduzir (somar tudo)
+
+const numerosPares = numeros
+  .filter((numeros) => numeros % 2 === 0)
+  .map((n) => n * 2)
+  .reduce((cont, n) => {
+    cont += n;
+    return cont;
+  });
+
+  console.log(numerosPares)

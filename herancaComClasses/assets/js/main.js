5a22276c1090785ed0d1266f72aae10bc9b0a3f4
Karen Christina <karen.santana@santander.com.br>
2021-12-01 08:14:10 -0300

    herança com classes (extends)

---
+++ herancaComClasses/assets/js/main.js
@@ -0,0 +1,32 @@
+class DispositivoEletronico {
+  constructor(nome) {
+    this.nome = nome;
+    this.ligado = false;
+  }
+
+  ligar() {
+    if (this.ligado) {
+      console.log(this.nome + " já ligado");
+      return;
+    }
+  }
+
+  desligar() {
+    if (!this.ligado) {
+      console.log(this.nome + " desligado");
+      return;
+    }
+    this.ligado = false;
+  }
+}
+
+class Smartphone extends DispositivoEletronico {
+  constructor(nome, cor, modelo) {
+    super(nome);
+    this.cor = cor;
+    this.modelo = modelo;
+  }
+}
+
+const s1 = new Smartphone("iPhone", "Preto", "Galaxy");
+console.log(s1);
